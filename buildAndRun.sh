ab691003952fc81d94b6f440f46023933d3c5dfc
orkun34 <orkun.akgul34@gmail.com>
2018-12-03 13:28:30 +0300

    initial commit
    initial commit

--- buildAndRun.sh
+++ buildAndRun.sh
@@ -1,6 +1,7 @@
 mvn clean install
-docker-compose build
-docker-compose up
+docker-compose up nebula-cont-mysql
+docker-compose up nebula-cont-springboot
+docker-compose up nebula-cont-rabbitmq
 #docker run -p 3366:3366 --name docker-mysql -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=adventureworks -e MYSQL_USER=nebula -e MYSQL_PASSWORD=password -d nebula_nebula-mysql:latest
 #docker run -p 8087:8087 --name nebula-app -d nebula_nebula-app:latest link docker-mysql
 #docker run -p 5666:5666 --name rabbitmq-app -d
